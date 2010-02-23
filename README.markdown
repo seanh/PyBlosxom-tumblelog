767920e7762c43665024c7b23b0257d16ba0900c
Sean Hammond <seanhammond@lavabit.com>
2010-02-23 23:08:19 +0000

    Rewrite of the tumblelog plugin, now uses its own file formats and parse functions instead of using YAML.

--- README.markdown
+++ README.markdown
@@ -1,62 +1,23 @@
 A Tumblelog Plugin for PyBlosxom
 ================================
 
-A tumblelog is a kind of blog in which different types of post can have
-different structures and templates, and all are rendered in one anarchic
-stream. For example, text posts have a title and some body text (like
-normal blog posts), link posts have the url being linked to, the link
-text, and a comment (like in a linklog), picture posts have an image
-file and a caption, a microblog post like on twitter or identica is
-just a snippet of text without even a title, etc. You can make up
-whatever types of post you want. For examples, check out tumblelog sites
-like tumblr.com.
+This is an entryparser plugin that handles several types of entry file:
+*.text, *.link, *.quote, *.picture, *.video. A file format is defined
+for each new entry type, these are simple and inspired by PyBlosxom's
+default file format. See `entries/` for examples of the tumblelog file
+formats in use.
 
-This tumblelog plugin is an entryparser plugin for PyBlosxom that lets
-you write entries in the structured markup language [YAML][], and lets
-each entry file specify which entry template it will be rendered with.
-Your flavour dir will contain lots of entry templates, one for each type
-of entry that you use, instead of just one 'story' template per flavour
-as is usual with PyBlosxom. Here is an [example of some PyBlosxom posts rendered using this plugin](http://dl.dropbox.com/u/136038/pyblosxom_tumblelog_plugin.html).
+Instead of using the `story` template each type of entry is rendered
+with its own template: `text`, `link`, `quote`, `picture`, `video`. See
+`flavours/` for example flavour templates.
 
-[YAML]: http//yaml.org/
+Markdown syntax is supported (e.g. in the bodies of text and link
+entries, in quotes, in picture captions, etc.) if python-markdown is
+installed on your system (or the python-markdown source is in your
+plugins directory).
 
-This plugin handles `*.yaml` entry files in your data dir. These should
-be YAML-formatted text files, that's the only rule, you can create
-whatever YAML forms you want. Well, not quite, your YAML must contain a
-key, value pair in which the key is `template_name` and the value is the
-name of the template to render this entry with. For example, here's the
-YAML file for a link post:
-
-    template_name: link
-    url: http://crypto.stanford.edu/~blynn/c/index.html
-    title: C Craft
-    comment: >    
-        An opinionated free online book about the craft of C programming,
-        looks good.
-
-and here is the corresponding `link` template from my flavour:
-
-    <div class="entry link">
-        <div class="date">
-            $(date)
-        </div>
-        <div class="content">
-            <a href="$(url)">$(title)</a><br/>
-            $(comment)
-        </div>
-    </div>
-
-as you can see, the template file references the same keys that were
-used in the entry file (url, title and comment). You can make up
-whatever keys you want and use them in your posts and templates, but
-it's up to you to ensure that your YAML entry files contain the same
-keys as used in their corresponding templates. You can
-make up whatever template files you want: links, quotes, pictures,
-books, and then make entry files for them. For more examples have a look
-at the example
-[entries](http://github.com/seanh/PyBlosxom-tumblelog/tree/master/entries/)
-and
-[flavour](http://github.com/seanh/PyBlosxom-tumblelog/tree/master/flavours/html.flav/)
-in this repo.
-
-_The tumblelog plugin was written by seanh with help from willkg._
+You could easily add a new entry type by adding a new
+`parse_my_entry_type` function in `plugins/tumblelog.py` following the
+example of the `parse_*` functions that are already there. You would
+need to add a template for the new entry type to each of your flavours
+too.
--- plugins/tumblelog.py
+++ plugins/tumblelog.py
@@ -1,23 +1,197 @@
 #!/usr/bin/env python
 """tumblelog.py -- a tumblelog plugin for PyBlosxom.
 
+TODO
+
+*   Improve the HTML templates, make sure they're valid HTML and CSS and use
+    the relevant HTML features.
+
+*   Write templates for each tumblelog entry type for the rest of the default
+    flavours: RSS, RSS 2, Atom, Error.
+
+*   Support optional click-through links in picture entries like tumblr does.
+
+*   Write more entry types: chat? audio? tweet? code snippet?
+
+*   Make all of the parse functions more robust to IOErrors and invalid
+    entry files. Instead of failing silently or crashing (and bringing down
+    pyblosxom) revert to an error template.
+
+*   Use YAML-style 'key: value' syntax for metadata instead of pyblosxom's '#key value',
+    and allow multi-line metadata entries by indentation.
+
+*   Get rid of some code duplication.
+
 """
 import os
-import yaml # PyYAML, which ships with this plugin.
+
+try:
+    import markdown 
+    md = markdown.Markdown(
+        output_format = 'html4',
+        # codehilite requires python-pygments
+        extensions = ['extra','codehilite'])
+except ImportError:
+    md = None
+
+def convert(text):
+    if md:
+        try:
+            html = md.convert(text)
+            md.reset()
+            return html
+        except markdown.MarkdownException:
+            # FIXME: Log the error with pyblosxom's logger instance.
+            pass
+    return text
 
 def cb_entryparser(entryparsingdict):
     """
-    Register this plugin's `parse` function as the parser function
-    for YAML entries.
+    Register this plugin's `parse_*` functions as the parser functions
+    for their corresponding entry filename extensions.
         
     """
-    entryparsingdict['yaml'] = parse    
+    entryparsingdict['text'] = parse_text
+    entryparsingdict['link'] = parse_link
+    entryparsingdict['picture'] = parse_picture
+    entryparsingdict['quote'] = parse_quote
+    entryparsingdict['video'] = parse_video
     return entryparsingdict
 
-def parse(filename, request):
-    """
-    Parse the YAML file.
+
+def parse_text(filename, request):
     
-    """
-    entrydata = yaml.load(open(filename,'r').read())    
-    return entrydata
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+    
+    if len(lines) == 0:
+        return {"title": "", "body": ""}
+    
+    entry_data = {'template_name':'text'}
+    
+    title = lines.pop(0).strip()
+    entry_data['title'] = title
+    
+    while lines and lines[0].startswith("#"):
+        meta = lines.pop(0)
+        meta = meta[1:].strip()     # remove the hash
+        meta = meta.split(" ", 1)
+        entry_data[meta[0].strip()] = meta[1].strip()
+
+    body = ''.join(lines)
+    entry_data['body'] = convert(body)
+
+    return entry_data
+
+def parse_link(filename, request):
+    
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+    
+    if len(lines) == 0:
+        return {"title": "", "body": ""}
+    
+    entry_data = {'template_name':'link'}
+    
+    title = lines.pop(0).strip()
+    entry_data['title'] = title
+
+    href = lines.pop(0).strip()
+    entry_data['href'] = href
+    
+    while lines and lines[0].startswith("#"):
+        meta = lines.pop(0)
+        meta = meta[1:].strip()     # remove the hash
+        meta = meta.split(" ", 1)
+        entry_data[meta[0].strip()] = meta[1].strip()
+
+    body = ''.join(lines)
+    entry_data['body'] = convert(body)
+
+    return entry_data
+
+def parse_picture(filename, request):
+    
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+    
+    if len(lines) == 0:
+        return {"title": "", "body": ""}
+    
+    entry_data = {'template_name':'picture'}
+    
+    title = lines.pop(0).strip()
+    entry_data['title'] = title
+
+    src = lines.pop(0).strip()
+    entry_data['src'] = src
+    
+    while lines and lines[0].startswith("#"):
+        meta = lines.pop(0)
+        meta = meta[1:].strip()     # remove the hash
+        meta = meta.split(" ", 1)
+        entry_data[meta[0].strip()] = meta[1].strip()
+
+    body = ''.join(lines)
+    entry_data['body'] = convert(body)
+
+    return entry_data
+
+def parse_quote(filename, request):
+    
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+    
+    if len(lines) == 0:
+        return {"title": "", "body": ""}
+    
+    entry_data = {'template_name':'quote'}
+    
+    quote = ''
+    while lines and lines[0].startswith(">"):
+        line = lines.pop(0)[1:]
+        quote = quote + line
+    entry_data['quote'] = convert(quote)
+
+    cite = '<cite>' + lines.pop(0).strip() + '</cite>'
+    entry_data['cite'] = convert(cite)
+
+    while lines and lines[0].startswith("#"):
+        meta = lines.pop(0)
+        meta = meta[1:].strip()     # remove the hash
+        meta = meta.split(" ", 1)
+        entry_data[meta[0].strip()] = meta[1].strip()
+
+    body = ''.join(lines)
+    entry_data['body'] = convert(body)
+
+    return entry_data
+
+def parse_video(filename, request):
+    
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+    
+    if len(lines) == 0:
+        return {"title": "", "body": ""}
+    
+    entry_data = {'template_name':'video'}
+    
+    embed = lines.pop(0).strip()
+    entry_data['embed'] = embed
+
+    while lines and lines[0].startswith("#"):
+        meta = lines.pop(0)
+        meta = meta[1:].strip()     # remove the hash
+        meta = meta.split(" ", 1)
+        entry_data[meta[0].strip()] = meta[1].strip()
+
+    body = ''.join(lines)
+    entry_data['body'] = convert(body)
+
+    return entry_data
